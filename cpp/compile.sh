880dd08f373b398335263ba1bed10c889496b03b
Akasan <daisuke1024akagawa@gmail.com>
2019-10-28 10:39:31 +0900

    add Makefile for using make and modify .gitignore

--- cpp/compile.sh
+++ cpp/compile.sh
@@ -2,6 +2,6 @@
 
 rm *.o
 g++ -c agent.cpp
-g++ -c ant_system.cpp
+#g++ -c ant_system.cpp
 g++ -c main.cpp
-g++ -o main main.o agent.o ant_system.o -std=c++17
+g++ -o main main.o agent.o #ant_system.o -std=c++17
